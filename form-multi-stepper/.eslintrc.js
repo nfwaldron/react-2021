6dc2801d72981c31605b16ae27c916de15ddd3db
Nathan Waldron <nathanwaldron@Jamilyahs-Air.fios-router.home>
2021-07-11 19:48:18 -0400

    setup eslint with typescript

--- form-multi-stepper/.eslintrc.js
+++ form-multi-stepper/.eslintrc.js
@@ -1,64 +1,123 @@
 module.exports = {
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2020,
+    // Can I remove these now?
+    ecmaFeatures: {
+      impliedStrict: true,
+      classes: true,
+    },
+  },
   env: {
     browser: true,
-    es2021: true,
+    node: true,
+    jquery: true,
+    jest: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint'],
   rules: {
-    '@typescript-eslint/ban-ts-ignore': 'off',
-    '@typescript-eslint/no-inferrable-types': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    semi: 'off', // must disable base rule to enable typescript version
-    '@typescript-eslint/semi': ['warn'],
-    'no-unused-vars': 'off', // must disable base rule to enable typescript version
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        ignoreRestSiblings: true,
-        args: 'after-used',
-      },
+    'no-debugger': 0,
+    'no-alert': 0,
+    'no-await-in-loop': 0,
+    'no-return-assign': ['error', 'except-parens'],
+    'no-restricted-syntax': [
+      2,
+      'ForInStatement',
+      'LabeledStatement',
+      'WithStatement',
     ],
-    'no-undef': 'off', // ignores for js config files
-    'no-multiple-empty-lines': [
+    'no-unused-vars': [
       1,
       {
-        max: 1,
-        maxBOF: 0,
+        ignoreRestSiblings: true,
+        argsIgnorePattern: 'res|next|^err',
       },
     ],
-    'comma-dangle': [
+    'prefer-const': [
       'error',
       {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'never',
+        destructuring: 'all',
       },
     ],
-    'react/display-name': 'off',
-    'react/no-unescaped-entities': 'off',
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'max-len': ['warn', { code: 140 }],
-    'no-console': 'error',
-    'ordered-imports': 'off',
-    quotes: ['warn', 'single'],
-    'sort-keys': 'off',
+    'arrow-body-style': [2, 'as-needed'],
+    'no-unused-expressions': [
+      2,
+      {
+        allowTaggedTemplates: true,
+      },
+    ],
+    'no-param-reassign': [
+      2,
+      {
+        props: false,
+      },
+    ],
+    'no-console': 0,
+    'import/prefer-default-export': 0,
+    import: 0,
+    'func-names': 0,
+    'space-before-function-paren': 0,
+    'comma-dangle': 0,
+    'max-len': 0,
+    'import/extensions': 0,
+    'no-underscore-dangle': 0,
+    'consistent-return': 0,
+    'react/display-name': 1,
+    'react/no-array-index-key': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/prefer-stateless-function': 0,
+    'react/forbid-prop-types': 0,
+    'react/no-unescaped-entities': 0,
+    'jsx-a11y/accessible-emoji': 0,
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        assert: 'either',
+      },
+    ],
+    'react/require-default-props': 0,
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    radix: 0,
+    'no-shadow': [
+      2,
+      {
+        hoist: 'all',
+        allow: ['resolve', 'reject', 'done', 'next', 'err', 'error'],
+      },
+    ],
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 80,
+        //below line only for windows users facing CLRF and eslint/prettier error
+        // non windows users feel free to delete it
+        endOfLine: 'auto',
+      },
+    ],
+    'jsx-a11y/href-no-hash': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
+      {
+        aspects: ['invalidHref'],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
+  plugins: ['html', 'prettier', 'react-hooks'],
 };
